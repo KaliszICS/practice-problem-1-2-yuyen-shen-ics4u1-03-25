6145b13b65931734dff99815d70ffbf055ec6553
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-03-04 19:15:22 +0000

    update notes

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,42 +1,31 @@
 
 class Notes {
 	public static void main(String[] args) {
+		
 		//Try Catch
 
-		//Try code block - if at any point it throws an exception we jump to the catch block
+		//Try code block - if at any point your code throws an exception, we jump to catch instead of crashing
+
 		int arr[] = new int[2];
-
-		try {
-			//index out of bounds
-			arr[-1] = 5; //as soon as an exception is reached, the rest of the try code is not executed.
-			
-			
-			int a = 10/0;
-
-			int num4 = Integer.parseInt("Hello");
+		try { //as soon as an exception happens, the rest of the try is skipped
+			arr[-1] = 0;
+			int a = 5/0;
+			int num = Integer.parseInt("Hello");
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(e);
+		}
+		catch (ArithmeticException e) {
+			System.out.println("YOU CANT DIVIDE BY ZEROOOO");
+		}
+		catch (NumberFormatException e) {
+			System.out.println("You can only enter numberrrsss");
+		}
+		catch (Exception e) { //avoid if you know what exceptions to expect
+			System.out.println(e);
 		}
 
+		System.out.println("Code after exception");
 
-		// System.out.println("3"); - code cannot come between try and catch
-		
-		//Catch runs only when an exception is thrown in the try block
-		
-		//catches are checked in order when an exception is thrown, so the bottom ones will not execute if a top one deals with it first
-			
-		catch (ArithmeticException aE) {
-			System.out.println("You tried to divide by zero");
-		}
-		catch (ArrayIndexOutOfBoundsException aIE){
-			System.out.println(aIE);
-			System.out.println("Haha your code failed");
-		}
-		catch (NumberFormatException nFE) {
-			System.out.println("Thats not an int");
-		}
-		catch (Exception e) { //works kind of like an else that catches all remaining exceptions
-			System.out.println("Some exception");
-		}
-
-		System.out.println("My code is still running");
 	}
 }
